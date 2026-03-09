313be10c219ac8969ae81b9c7cc5534286913a2b
Andy Ramiandrisoa <andy.ramiandrisoa.pro@gmail.com>
2026-03-09 18:58:46 +0100

    calendrier annuel

--- monthly.py
+++ monthly.py
@@ -56,20 +56,27 @@ def month1(year, months : list):
 
     c.showPage()
     
-    doted_canvas(c,1, 0.01 )
+    doted_canvas(c,space, size )
     for i,month in enumerate(months[6:]):
-        draw_month_calendar(c, year, month,3*cm, 3*cm+ (9*cm*i)) 
+        #draw_month_calendar(c, year, month,3*cm, 3*cm+ (9*cm*i)) 
         c.line( 3*cm, 10*cm+ (9*cm*i), LARGEUR-3*cm, 10*cm+ (9*cm*i))
 
 
 if __name__ == "__main__":
     c = canvas.Canvas("monthly2.pdf", bottomup=0)
-    #doted_canvas(c,1, 0.01 )
-    year = 2025
-    month = 3
-    current_week = 1
-    draw_month_calendar(c, year, month,5*cm , 6*cm)
-    c.line(LARGEUR//2,3*cm, LARGEUR//2, HAUTEUR*2 - 6 *cm)
-    c.save()
+    #doted_canvas(c)
+    months = [i+1 for i in range(12)]
+    year = 2026
+    for i,month in enumerate(months[:6]):
+        draw_month_calendar(c, year, month,3*cm, 3*cm+ (9*cm*i)) 
+        c.line( 3*cm, 10*cm+ (9*cm*i), LARGEUR-3*cm, 10*cm+ (9*cm*i))
     
 
+    c.showPage()
+    
+    #doted_canvas(c,1, 0.01 )
+    for i,month in enumerate(months[6:]):
+        draw_month_calendar(c, year, month,3*cm, 3*cm+ (9*cm*i)) 
+        c.line( 3*cm, 10*cm+ (9*cm*i), LARGEUR-3*cm, 10*cm+ (9*cm*i))
+    c.showPage()
+    c.save()
